19beec8ab0deea70b9648454d624ffc6097ca0d3
عزالدين ناصر <121424065+izzeldeenn@users.noreply.github.com>
2026-04-05 21:00:00 +0200

    اضافة

--- src/components/study/ChallengesButton.tsx
+++ src/components/study/ChallengesButton.tsx
@@ -190,7 +190,6 @@ export function ChallengesButton() {
 
   const renderChallengesModal = () => (
     <>
-      {renderChallengesButton()}
       {isChallengesOpen && (
         <div className="fixed inset-0 z-50 flex items-center justify-center p-4">
           {/* Backdrop */}

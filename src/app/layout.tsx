ddfcc98c3e447e61f8591faf1609ded837662876
عزالدين ناصر <121424065+izzeldeenn@users.noreply.github.com>
2026-04-21 07:35:42 +0200

    اضافة احصائيات جوجل

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -78,6 +78,15 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.googleapis.com"/>
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous"/>
         <link href="https://fonts.googleapis.com/css2?family=ADLaM+Display&family=Amiri:ital,wght@0,400;0,700;1,400;1,700&family=Tajawal:wght@200;300;400;500;700;800;900&display=swap" rel="stylesheet"/>
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-H148YKHBMV"></script>
+        <script dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-H148YKHBMV');
+          `
+        }} />
       </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
